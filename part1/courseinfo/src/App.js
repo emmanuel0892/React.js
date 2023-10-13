bc2d5a09db12e94d2653b3bf5dead5520a84d202
emmanuel0892 <emmanuel.rubio.gaete92@gmail.com>
2023-10-13 00:49:00 -0300

    ejercicio 1.4 realizado

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,4 +1,4 @@
-function Header(props) {
+const Header = (props) => {
   return (
     <>
       <h1>{props.course}</h1>
@@ -6,69 +6,73 @@ function Header(props) {
   )
 }
 
-function Content(props) {
+const Content = (props) => {
 
+  const nombres = props.parts.map(n => n.name)
+  const cant = props.parts.map(c => c.exercises)
 
   return (
     <>
       <div>
-        <Part part1={props.part1} exercises1={props.exercises1}/>
-        <Part part2={props.part2} exercises2={props.exercises2}/>
-        <Part part3={props.part3} exercises3={props.exercises3}/>
+        <Part nombres={nombres} cant={cant} />
       </div>
     </>
   )
 }
 
-function Part(props) {
+const Part = (props) => {
   return (
     <>
       <p>
-        {props.part1} {props.exercises1}
+        {props.nombres[0]} {props.cant[0]}
       </p>
       <p>
-        {props.part2} {props.exercises2}
+        {props.nombres[1]} {props.cant[1]}
       </p>
       <p>
-        {props.part3} {props.exercises3}
+        {props.nombres[2]} {props.cant[2]}
       </p>
     </>
   )
 }
 
-function Total(props) {
+const Total = (props) => {
+
+  const exercises = props.parts.map(p => p.exercises)
+  
   return (
     <>
-      <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+      <p>Number of exercises {parseInt(exercises[0] + exercises[1] + exercises[2])} </p>
     </>
   )
 }
 
-function App() {
+const App = () => {
 
-  const course     = 'Half Stack application development'
-  const part1      = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2      = 'Using prps to pass data'
-  const exercises2 = 7
-  const part3      = 'State of a component'
-  const exercises3 = 14
+  const course = 'Half Stack application development'
+
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using prps to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
   return (
     <>
 
       <div>
         <Header course={course}/>
-        <Content 
-          part1={part1} exercises1={exercises1} 
-          part2={part2} exercises2={exercises2}
-          part3={part3} exercises3={exercises3}
-        />
-        <Total 
-          exercises1={exercises1} 
-          exercises2={exercises2} 
-          exercises3={exercises3}
-        />
+        <Content parts={parts} />
+        <Total parts={parts} />
       </div>
 
     </>
