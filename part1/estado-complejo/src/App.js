4c4e84830dbaae0799a62b1db33973403f75bdec
emmanuel0892 <emmanuel.rubio.gaete92@gmail.com>
2023-10-17 15:06:40 -0300

    actividad finalizada

--- part1/estado-complejo/src/App.js
+++ part1/estado-complejo/src/App.js
@@ -1,7 +1,22 @@
-function App() {
+import { useState } from "react";
+
+const App = () => {
+  const [ clicks, setClicks ] = useState({
+    left:0, right:0
+  })
+
+  const handleLeftClicks = () => 
+    setClicks({ ...clicks, left: clicks.left  + 1 })
+
+  const handleRightClicks = () => 
+    setClicks({ ...clicks, right: clicks.right + 1 })
+
   return (
     <div>
-      
+      {clicks.left}
+      <button onClick={handleLeftClicks}>Left</button>
+      <button onClick={handleRightClicks}>Right</button>
+      {clicks.right}
     </div>
   );
 }
